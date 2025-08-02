72a35e2d4d7ed16adb19d6d5e1b2c8f077d0cf14
Louis Laskar Ginting <louisginting54@gmail.com>
2025-08-02 17:05:08 +0700

    update vercel

--- api/index.php
+++ api/index.php
@@ -1,2 +1,23 @@
-<?php 
-    require __DIR__ . '/../public/index.php';
+<?php
+
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Check if the application exists
+if (file_exists(__DIR__.'/../vendor/autoload.php')) {
+    require __DIR__.'/../vendor/autoload.php';
+} else {
+    die('Autoload file not found. Please run composer install.');
+}
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
